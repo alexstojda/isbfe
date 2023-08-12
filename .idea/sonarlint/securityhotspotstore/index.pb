69ab75fd0579fdf323807959cd31e7fb22c17758
Alex <1353716+alexstojda@users.noreply.github.com>
2023-08-12 11:49:22 -0400

    feat: do the thing

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,12 +1,8 @@
 
-=
-src/index.css,c/5/c5d38a62d1faae90a2acaa34b79a7ef365c917e4
 A
 public/index.html,f/4/f43ecdf8d666ee908aacce037b4cd72159e4741b
 A
 public/robots.txt,d/a/dac799070c2d0c2fa24462b02c1f2dcc3fb3a1e9
-;
-src/App.css,a/a/aa7165aefcf8ee3d73539e99ab007daf1aa18d8d
 :
 
 src/App.js,1/e/1e43fa63aecd2127be034b3fb9d4caa172275714
@@ -14,8 +10,6 @@ D
 public/manifest.json,4/2/42e89cd2213b9d5dfae4f63522702c623170d792
 F
 src/reportWebVitals.js,1/c/1ce5e63ae1582ed0b425c46001299a07515efbf9
-<
-src/logo.svg,6/d/6d3a8cd532ece5b4ddbd4b3b6a678ba686d300bc
 ?
 src/App.test.js,9/2/923703ced91ac38943d37930d9d27a7ebe8f01ad
 A
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import QRCode from "react-qr-code";
+import topImg from './top.png'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [renderScreenshot, setRenderScreenshot] = React.useState(false);
+  const [ban, setBan] = React.useState<number>();
+  const [qrValue, setQrValue] = React.useState<string>();
+
+  useEffect(() => {
+    if (ban) {
+      const data = {
+        "ban": `"${ban}"`,
+        "eventID":"ÎLESONIQ"
+      }
+      // base64 encode data into string
+      const dataString = btoa(JSON.stringify(data));
+      setQrValue(`01_${dataString}`)
+    }
+  }, [ban]);
+
+  if (renderScreenshot && qrValue) {
+    return (
+      <>
+        <center>
+          <p>Screenshot and crop between the lines</p>
+          <hr style={{margin: '1em 0em'}} />
+          <img alt={'top'} src={topImg} width={'500'} />
+          <div style={{height: "auto", margin: "0 auto", maxWidth: 275, width: "100%"}}>
+            <QRCode
+              size={256}
+              style={{height: "auto", maxWidth: "100%", width: "100%"}}
+              value={qrValue}
+              viewBox={`0 0 256 256`}
+            />
+          </div>
+          <br/>
+          <hr style={{marginTop: '4em'}}/>
+          <input type={'button'} value={'Back'} onClick={() => setRenderScreenshot(false)}/>
+        </center>
+      </>
+    );
+  } else {
+    return (
+      <center>
+        <div style={{marginTop: '2em', width: '15em'}}>
+          <input
+            type={'number'} value={ban}
+            min={'200000000'}
+            max={'599999999'}
+            onChange={
+              (e) => setBan(parseInt(e.target.value))
+            }
+            placeholder={'enter number'}
+            style={{width: '100%', height: '2em'}}
+          />
+          <br/>
+          <input
+            type={'button'}
+            value={'Generate Screenshot'}
+            onClick={() => setRenderScreenshot(true)}
+            style={{width: '100%', height: '2em', marginTop: '1em'}}
+          />
+        </div>
+      </center>
+    )
+  }
 }
 
 export default App;
